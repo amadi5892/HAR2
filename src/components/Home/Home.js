b4d0cf111e37cb0e7552e3226bbda3960c427b69
Parrish Williamson <williamsonparrish@gmail.com>
2021-03-01 19:44:43 -0500

    styled navigation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,13 @@ import { render } from "@testing-library/react";
 
 import React, {Component}  from 'react';
 
+import './Home.css';
+
 class Home extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="home-container">
+
             </div>
         );
     }
